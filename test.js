77c290574ec3df5511787bcdc0edd7e250bf6641
Philip Koller <philip.koller@outlook.com>
2023-01-12 22:50:40 -0600

    added accessibility scan test

--- test.js
+++ test.js
@@ -1,4 +1,4 @@
-const {Builder, By, until} = require('selenium-webdriver');
+const { Builder, By, until } = require('selenium-webdriver');
 const AxeBuilder = require('@axe-core/webdriverjs');
 
 let driver;
@@ -11,7 +11,13 @@ describe('Accessibility Tests', () => {
         await driver.get('https://dequeuniversity.com/demo/mars');
         await driver.findElement(By.css('main-nav'))
     });
-
+    
+    it('Perform accessibility scan of the page', async () => {
+        await driver.get('https://dequeuniversity.com/demo/mars');
+        const results = await new AxeBuilder(driver).analyze();
+        console.log(results);
+        expect(results.violations.length).toBe(0);
+    });
 });
 
 afterAll(async () => {
